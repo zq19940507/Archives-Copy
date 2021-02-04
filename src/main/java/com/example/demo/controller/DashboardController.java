ead2d031feeddee1bb66ae7870823013be2cce3e
Zq19940507 <784542828@qq.com>
2021-02-04 16:53:33 +0800

    0204

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by 主页面 on 2021/2/3 10:32
  */
 @Controller
+
 public class DashboardController {
     @GetMapping("/user/dashboard")
     public String Dash(){
@@ -16,6 +18,11 @@ public class DashboardController {
     @GetMapping("/user/record1")
     public  String recodr1(){
         return  "record/record1";
+    }
+
+    @GetMapping("/user/mail")
+    public  String mail(){
+        return "mail";
     }
 
 }
